ac89500859bc11c2f45c133d483ac49e09af94f0
Ijonas Kisselbach <ijonas@ijonas.com>
2022-05-21 17:18:22 +0100

    Polygon mainnet jobs

---
+++ polygon-mainnet/Get > Bytes/example.sol
@@ -0,0 +1,73 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+
+/**
+ * @notice DO NOT USE THIS CODE IN PRODUCTION. This is an example contract.
+ */
+contract GenericLargeResponse is ChainlinkClient {
+    using Chainlink for Chainlink.Request;
+
+    // variable bytes returned in a single oracle response
+    bytes public data;
+    string public stringData;
+    uint256 private constant ORACLE_PAYMENT = (LINK_DIVISIBILITY / 100) * 7; // 0.07LINK
+
+    string constant jobId = "a84b561bd8f64300a0832682f208321f";
+
+    /**
+     * @notice Initialize the link token and target oracle
+     * @dev The oracle address must be an Operator contract for multiword response
+     */
+    constructor() {
+        // POLYGON MAINNET
+        setChainlinkToken(0xb0897686c545045aFc77CF20eC7A532E3120E0F1);
+        setChainlinkOracle(0x188b71C9d27cDeE01B9b0dfF5C1aff62E8D6F434);
+    }
+
+    /**
+     * @notice Request variable bytes from the oracle
+     */
+    function requestBytes(string memory _url, string memory _path) public {
+        Chainlink.Request memory req = buildChainlinkRequest(
+            stringToBytes32(jobId),
+            address(this),
+            this.fulfillBytes.selector
+        );
+        req.add("get", _url);
+        req.add("path", _path);
+        sendOperatorRequest(req, ORACLE_PAYMENT);
+    }
+
+    event RequestFulfilled(bytes32 indexed requestId, bytes indexed data);
+
+    /**
+     * @notice Fulfillment function for variable bytes
+     * @dev This is called by the oracle. recordChainlinkFulfillment must be used.
+     */
+    function fulfillBytes(bytes32 requestId, bytes memory bytesData)
+        public
+        recordChainlinkFulfillment(requestId)
+    {
+        emit RequestFulfilled(requestId, bytesData);
+        data = bytesData;
+        stringData = string(data);
+    }
+
+    function stringToBytes32(string memory source)
+        private
+        pure
+        returns (bytes32 result)
+    {
+        bytes memory tempEmptyStringTest = bytes(source);
+        if (tempEmptyStringTest.length == 0) {
+            return 0x0;
+        }
+
+        assembly {
+            // solhint-disable-line no-inline-assembly
+            result := mload(add(source, 32))
+        }
+    }
+}
