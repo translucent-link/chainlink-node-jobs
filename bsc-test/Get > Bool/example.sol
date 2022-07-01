3b86689d3d04f5aaf9c563856793c4471ae5ea45
Ijonas Kisselbach <ijonas@ijonas.com>
2022-07-01 20:28:07 +0100

    minor fixes after testing on BSC Testnet

--- bsc-test/Get > Bool/example.sol
+++ bsc-test/Get > Bool/example.sol
@@ -7,14 +7,14 @@ import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
 /**
  * @notice DO NOT USE THIS CODE IN PRODUCTION. This is an example contract.
  */
-contract GetUint256 is ChainlinkClient, ConfirmedOwner {
+contract GetBool is ChainlinkClient, ConfirmedOwner {
     using Chainlink for Chainlink.Request;
 
     uint256 private constant ORACLE_PAYMENT =
         ((1 * LINK_DIVISIBILITY) / 100) * 5;
     bool public value;
 
-    event RequestValue(bytes32 indexed requestId, uint256 indexed value);
+    event RequestValue(bytes32 indexed requestId, bool indexed value);
 
     string constant jobId = "a75ebb6e5a7b492fbfb67ab240100a95"; // BSC Test
 
@@ -24,11 +24,11 @@ contract GetUint256 is ChainlinkClient, ConfirmedOwner {
         setChainlinkOracle(0x4246103C6fF2e3eC839751156b518d066aab5e5A);
     }
 
-    function requestValue(
-        string memory _url,
-        int256 _multiply,
-        string memory _path
-    ) public onlyOwner returns (bytes32 requestId) {
+    function requestValue(string memory _url, string memory _path)
+        public
+        onlyOwner
+        returns (bytes32 requestId)
+    {
         Chainlink.Request memory req = buildChainlinkRequest(
             stringToBytes32(jobId),
             address(this),
