86cb9c623861cabb8fd8d5a3e090d57ac71cd3cf
Ijonas Kisselbach <ijonas@ijonas.com>
2022-12-03 22:37:05 +0000

    IPFS on Ethereum

---
+++ ethereum-mainnet/IPFS > Bool/example.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
+
+/**
+ * @notice DO NOT USE THIS CODE IN PRODUCTION. This is an example contract.
+ */
+contract IpfsBool is ChainlinkClient, ConfirmedOwner {
+    using Chainlink for Chainlink.Request;
+
+    uint256 private constant ORACLE_PAYMENT = 1360000000000000000;
+    bool public value;
+
+    event RequestValue(bytes32 indexed requestId, bool indexed value);
+
+    bytes32 constant jobId = "cb8fb6d6a55243838424d91b412daf61";
+
+    constructor() ConfirmedOwner(msg.sender) {
+        // Ethereum
+        setChainlinkToken(0x514910771AF9Ca656af840dff83E8264EcF986CA);
+        setChainlinkOracle(0x188b71C9d27cDeE01B9b0dfF5C1aff62E8D6F434);
+    }
+
+    function requestValue(string memory _cid, string memory _path)
+        public
+        onlyOwner
+        returns (bytes32 requestId)
+    {
+        Chainlink.Request memory req = buildChainlinkRequest(
+            jobId,
+            address(this),
+            this.fulfillValue.selector
+        );
+        req.add("cid", _cid);
+        req.add("path", _path);
+        return sendChainlinkRequest(req, ORACLE_PAYMENT);
+    }
+
+    function fulfillValue(bytes32 _requestId, bool _value)
+        public
+        recordChainlinkFulfillment(_requestId)
+    {
+        emit RequestValue(_requestId, _value);
+        value = _value;
+    }
+}
